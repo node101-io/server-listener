0c09ab3fab36667f45b0a028538599ecc7afe807
root <root@vmi1114925.contaboserver.net>
2024-02-22 10:58:43 +0100

    modify draft

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 const ws = require('ws');
 
 const webSocket = new ws.WebSocket('ws://localhost:26657/websocket');
@@ -14,5 +15,10 @@ webSocket.on('open', () => {
 });
 
 webSocket.on('message', message => {
-  console.log(JSON.parse(message.toString()).result);
+  console.log(message.toString());
 });
+
+fetch('http://127.0.0.1:26657/tx?hash=0x5EC7224872FCCFBA28D4113A1369649555317B5F5EF25B3EFD5BF7F64398F1E8')
+  .then(res => res.json())
+  .then(console.log)
+
