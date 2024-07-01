eeff10b197a2fe302dbdbb8b1b33cda06c05af82
necipsagiro <sagiroglun22@itu.edu.tr>
2024-07-01 19:40:08 +0300

    chore: Update package.json version to 0.0.25 and remove unused dependencies

--- utils/decodeTxs.js
+++ utils/decodeTxs.js
@@ -1,47 +1,47 @@
-const async = require('async');
-const { decodeTxRaw, Registry } = require('@cosmjs/proto-signing');
-const { defaultRegistryTypes } = require('@cosmjs/stargate');
-// const { toHex } = require('@cosmjs/encoding');
-// const { sha256 } = require('@cosmjs/crypto');
+// const async = require('async');
+// const { decodeTxRaw, Registry } = require('@cosmjs/proto-signing');
+// const { defaultRegistryTypes } = require('@cosmjs/stargate');
+// // const { toHex } = require('@cosmjs/encoding');
+// // const { sha256 } = require('@cosmjs/crypto');
 
 
-const MESSAGE_TYPES_TO_DECODE = [
-  '/cosmos.bank.v1beta1.MsgSend',
-  '/cosmos.staking.v1beta1.MsgDelegate',
-  '/cosmos.staking.v1beta1.MsgUndelegate'
-  // TODO: Add more message types here
-];
+// const MESSAGE_TYPES_TO_DECODE = [
+//   '/cosmos.bank.v1beta1.MsgSend',
+//   '/cosmos.staking.v1beta1.MsgDelegate',
+//   '/cosmos.staking.v1beta1.MsgUndelegate'
+//   // TODO: Add more message types here
+// ];
 
-const registry = new Registry(defaultRegistryTypes);
+// const registry = new Registry(defaultRegistryTypes);
 
-module.exports = (txs, callback) => {
-  async.map(txs, (base64tx, callback) => {
-    const bufferTx = Buffer.from(base64tx, 'base64');
-    const tx = decodeTxRaw(bufferTx);
+// module.exports = (txs, callback) => {
+//   async.map(txs, (base64tx, callback) => {
+//     const bufferTx = Buffer.from(base64tx, 'base64');
+//     const tx = decodeTxRaw(bufferTx);
 
-    tx.body.messages = tx.body.messages.reduce((acc, message) => {
-      if (MESSAGE_TYPES_TO_DECODE.includes(message.typeUrl))
-        acc.push({
-          typeUrl: message.typeUrl,
-          value: registry.decode(message)
-        });
+//     tx.body.messages = tx.body.messages.reduce((acc, message) => {
+//       if (MESSAGE_TYPES_TO_DECODE.includes(message.typeUrl))
+//         acc.push({
+//           typeUrl: message.typeUrl,
+//           value: registry.decode(message)
+//         });
 
-      return acc;
-    }, []);
+//       return acc;
+//     }, []);
 
-    callback(null, {
-      hash: bufferTx, // TODO: hash in front toHex(sha256(bufferTx))
-      ...tx.body,
-    });
-  }, (err, decodedTxs) => {
-    if (err)
-      return callback(err);
+//     callback(null, {
+//       hash: bufferTx, // TODO: hash in front toHex(sha256(bufferTx))
+//       ...tx.body,
+//     });
+//   }, (err, decodedTxs) => {
+//     if (err)
+//       return callback(err);
 
-    return callback(null, decodedTxs.reduce((acc, tx) => {
-      if (tx.messages.length)
-        acc.push(tx);
+//     return callback(null, decodedTxs.reduce((acc, tx) => {
+//       if (tx.messages.length)
+//         acc.push(tx);
 
-      return acc;
-    }, []));
-  });
-};
+//       return acc;
+//     }, []));
+//   });
+// };
